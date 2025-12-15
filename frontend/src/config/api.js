e7c1a05f9d7edf38cef164c720a50c05d2d6f2d1
Vincenzo Dimino <vincenzodimino@yahoo.it>
2025-12-15 01:51:49 +0100

    Deploy frontend for Render static site

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,4 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL ||
-  "http://localhost:8080/api";
+  process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export default API_BASE_URL;
