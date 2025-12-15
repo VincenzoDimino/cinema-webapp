71ec88a61efdb61fb46ec79f7ba326482e92d4de
Vincenzo Dimino <vincenzodimino@yahoo.it>
2025-12-15 01:37:14 +0100

    Build frontend ready for production

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,4 +1,6 @@
 const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
+  process.env.NODE_ENV === "production"
+    ? "https://cinema-backend-ohhi.onrender.com/api"
+    : "http://localhost:8080/api";
 
 export default API_BASE_URL;
